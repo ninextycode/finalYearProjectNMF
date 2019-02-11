3e15543f705fea60690f18086f8d246c4a09360b
Ubuntu <aiadmin@AI.txinhz3p432eneylrjl5gsf0je.ax.internal.cloudapp.net>
2019-02-11 23:36:16 +0000

    Notebook & download script changes

--- data/downlodad_data.sh
+++ data/downlodad_data.sh
@@ -10,20 +10,21 @@ wget -nc http://www.cl.cam.ac.uk/Research/DTG/attarchive/pub/data/att_faces.zip
 wget -nc https://archive.ics.uci.edu/ml/machine-learning-databases/reuters21578-mld/reuters21578.tar.gz
 
 unzip 10_4231_R7RX991C.zip -d indian_pines
+mv indian_pines/10_4231_R7RX991C/* indian_pines 
 (cd indian_pines && unzip bundle.zip)
-(cd indian_pines/documentation &&\
-    unzip NS-line_Project_and_Ground_Reference_Files.zip &&
-    unzip Site3_Project_and_Ground_Reference_Files.zip)
-(cd indian_pines &&\
-    mkdir -p images &&\
-    mv documentation/19920612_AVIRIS_IndianPine_NS-line_gr.tif images &&\
-    mv documentation/Site3_Project_and_Ground_Reference_Files/19920612_AVIRIS_IndianPine_Site3_gr.tif images &&\
-    mv aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_NS-line.tif images
-    mv aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_Site3.tif images)
+(cd indian_pines/documentation && unzip NS-line_Project_and_Ground_Reference_Files.zip)
+(cd indian_pines/documentation && unzip Site3_Project_and_Ground_Reference_Files.zip)
+(cd indian_pines && mkdir -p images)
 
+(cd indian_pines && mv documentation/19920612_AVIRIS_IndianPine_NS-line_gr.tif images)
+(cd indian_pines && mv documentation/Site3_Project_and_Ground_Reference_Files/19920612_AVIRIS_IndianPine_Site3_gr.tif images)
+(cd indian_pines && mv aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_NS-line.tif images)
+(cd indian_pines && mv aviris_hyperspectral_data/19920612_AVIRIS_IndianPine_Site3.tif images)
+
+
+sudo apt-get install -y mmv 
 unzip att_faces.zip -d att_faces
-(cd att_faces &&\
-    mkdir -p images
-    mmv s\*/\* images/\#1_\#2)
+(cd att_faces && mkdir -p images)
+(cd att_faces && mmv s\*/\* images/\#1_\#2)
 
 mkdir -p reuters21578 && tar xkvfz reuters21578.tar.gz -C reuters21578
