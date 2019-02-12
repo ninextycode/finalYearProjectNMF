c971916dc5ca6c7844fe08c92cf5fc7ead474c8e
Maxim <ninexty@gmail.com>
2019-02-12 09:21:37 +0300

    Moved some performance functions to .py file

--- performance/performance_eval_func.py
+++ performance/performance_eval_func.py
@@ -67,3 +67,75 @@ def torch_algo_wrapper(algo, device="cuda"):
         result[1] = result[1].to("cpu").numpy()
         return result
     return algo_w
+
+
+def errors_at_time_t_over_inner_dim(V, r_range, t, algo_dict):
+    error_data = {algo_name: [] for algo_name in algo_dict.keys()}
+
+    for r in r_range:
+        W_init, H_init = nmf.mult.update_empty_initials(V, r, None, None)
+        for algo_name, algo in algo_dict.items():
+            W, H = algo(V=V, inner_dim=r,
+                        record_errors=False,
+                        time_limit=t,
+                        max_steps=np.inf,
+                        epsilon=0,
+                        W_init=W_init.copy(),
+                        H_init=H_init.copy())
+            error = nmf.norms.norm_Frobenius(V - W @ H)
+            error_data[algo_name].append([r, error])
+    return {k: np.array(v) for k, v in error_data.items()}
+
+
+def plot_errors_dict(dict_data, ax, log=False):
+    for k, v in dict_data.items():
+        ls = "--" if "torch" in k else "-"
+        y_data = np.log(v[:, 1]) if log else v[:, 1]
+        ax.plot(v[:, 0], y_data, label=k, ls=ls)
+    ax.legend()
+    return ax
+
+
+def plot_ratios_gpu_algo(errors, ax, base="mult_torch"):
+    ratios = get_time_ratio(errors[base], errors["nesterov_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="nesterov_torch")
+
+    ratios = get_time_ratio(errors[base], errors["mult_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="mult_torch")
+
+    ratios = get_time_ratio(errors[base], errors["pgrad_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="pgrad_torch")
+
+    ax.set_title("How faster is X than {} on GPU".format(base))
+    ax.invert_xaxis()
+    ax.legend()
+
+
+def plot_ratios_cpu_algo(errors, ax, base="mult"):
+    ratios = get_time_ratio(errors[base], errors["nesterov"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="nesterov")
+
+    ratios = get_time_ratio(errors[base], errors["mult"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="mult")
+
+    ratios = get_time_ratio(errors[base], errors["pgrad"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="pgrad")
+
+    ax.set_title("How faster is X than {} on CPU".format(base))
+    ax.invert_xaxis()
+    ax.legend()
+
+
+def plot_ratios_cpu_gpu(errors, ax):
+    ratios = get_time_ratio(errors["nesterov"], errors["nesterov_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="nesterov")
+
+    ratios = get_time_ratio(errors["mult"], errors["mult_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="mult")
+
+    ratios = get_time_ratio(errors["pgrad"], errors["pgrad_torch"])
+    ax.plot(ratios[:, 0], ratios[:, 1], label="pgrad")
+
+    ax.set_title("How faster is X on GPU than on CPU")
+    ax.invert_xaxis()
+    ax.legend()
